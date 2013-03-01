641630e3501a75839b588a947121bfa6e855ae51
Paul Dijou <pdi@zenexity.com>
2013-03-01 16:46:23 +0100

    Improve Search UI design

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,9 +5,11 @@ app.controller('SearchCtrl', ['$scope', '$routeParams', "Search", function($scop
     }
     $scope.data.result = Search.query({q: $scope.data.query}, function() {
         angular.forEach($scope.data.result.hits.hits, function(value) {
-            var gistId = value._source.url.match(/([0-9]+)$/)[1];
-            angular.element("#results ul").append('<li id="gist'+ gistId +'"></li>');
-            angular.element("#resultScripts").append('<script type="text/javascript" src="'+value._source.url+'.json?callback=displayGist"></script>')
+            var gistId = value._source.id || value._source.url.match(/([0-9]+)$/)[1];
+            var gistUrl = value._source.url || "https://gist.github.com/"+gistId;
+            var gistDescription = value._source.description;
+            angular.element("#results ul").append('<li><div class="description" data-toggle="collapse" data-target="#gist'+ gistId +'">'+ gistDescription +'</div><div id="gist'+ gistId +'" class="gist collapse in"></div></li>');
+            angular.element("#resultScripts").append('<script type="text/javascript" src="'+ gistUrl +'.json?callback=displayGist"></script>')
         })
     });
 }])

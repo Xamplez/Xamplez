b779ad4c80bd30360f35baa490ddf1d3a36323e0
Gaëtan Renaudeau <gre@zenexity.com>
2013-03-01 17:11:06 +0100

    Add fork button and improve style

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,9 +5,14 @@ import play.api.mvc._
 import services.github._
 import play.api.libs.concurrent.Execution.Implicits._
 
+import play.api.Play.current
+
 object Application extends GithubOAuthController {
+
+  val originalGistUrl = "https://gist.github.com/"+Play.application.configuration.getString("gist.root").get
+
   def main(any: String) = Action {
-    Ok(views.html.main())
+    Ok(views.html.main(originalGistUrl))
   }
 
   def notFound(any: String) = Action {

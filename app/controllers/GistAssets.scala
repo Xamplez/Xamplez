4f316efd23c58a35d56c80ca9cecce2b6f5a054b
Timshel <timshel@R700.(none)>
2013-07-11 20:54:17 +0200

    Add an ugly synchroset to invalidate cache

--- app/controllers/GistAssets.scala
+++ app/controllers/GistAssets.scala
@@ -7,6 +7,7 @@ import play.api.mvc._
 import play.api.Play.current
 
 import java.net.URLConnection
+import scala.collection._
 import scala.concurrent.Future
 
 import services.github.GithubWS._
@@ -14,11 +15,21 @@ import services.github.GithubWS._
 object GistAssets extends Controller{
   lazy val GIST_CONF = Play.application.configuration.getLong("config.external.gist")
 
+  val synchroSet = new mutable.HashSet[String] with mutable.SynchronizedSet[String]
+
+  def invalidate = Action{
+    val values = synchroSet.toSeq
+    synchroSet --= values
+    values.foreach { v => Cache.remove(v) }
+    Ok("Done")
+  }
+
   def at( fileName: String ) = Action{
     Async{
       (( Cache.getAs[String](fileName), GIST_CONF ) match {
         case ( None, Some(id) ) => Gist.getFile(id, fileName).map{ 
           case Some(file) => {
+            synchroSet.add(fileName)
             Cache.set(fileName, file)
             Some(file)
           }

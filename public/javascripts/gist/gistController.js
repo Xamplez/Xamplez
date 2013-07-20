22ce0bdf0c1776ba205da0283052353841309f31
Pascal Voitot <pascal.voitot.dev@gmail.com>
2013-07-20 21:16:53 +0200

    corrected gist display for full mode + style + distinct langs only in gists

--- public/javascripts/gist/gistController.js
+++ public/javascripts/gist/gistController.js
@@ -1,8 +1,7 @@
-app.controller('GistCtrl', ['$scope', '$routeParams', "$compile", 'GistService', 'Gists',
-	function($scope, $routeParams, $compile, GistService, Gists) {
+app.controller('GistCtrl', ['$scope', '$routeParams', "$compile", 'GistService', 'Gists', function($scope, $routeParams, $compile, GistService, Gists) {
 
-	$scope.gist = Gists.get({id: $routeParams.id});
-	$scope.comments = Gists.comments({id: $routeParams.id});
+  $scope.gist = Gists.get({id: $routeParams.id});
+  $scope.comments = Gists.comments({id: $routeParams.id});
 
   $scope.isStarred = function(id){
     return GistService.isStarred(id);
